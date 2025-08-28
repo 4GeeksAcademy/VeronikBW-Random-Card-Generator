e0e540b9a12b8f41d2753a78320ca8ad4985aa86
Verónica Rodríguez <veronikbw@gmail.com>
2025-08-28 20:24:45 +0000

    preliminar de la carta

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 import "bootstrap";
 import "./style.css";
+const inconsCard =  ["♦", "♥", "♠", "♣"]
+const numbersCard = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let body = document.body
+body.style.background = "green"
+body.style.height = "100vh"
+body.style.display = "flex"
+body.style.alignItems = "center"
+body.style.justifyContent = "center"
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let root = document.getElementById("root");
+root.classList.add("my-card")
+
+let divTop = document.createElement("div")
+let iconCard = inconsCard[Math.floor(Math.random()*inconsCard.length)]
+root.appendChild(divTop)
+divTop.innerHTML = iconCard
+divTop.style.fontSize = "50px"
+
+
+let divMiddle = document.createElement("div")
+root.appendChild(divMiddle)
+divMiddle.innerHTML =  numbersCard[Math.floor(Math.random()*numbersCard.length)]
+
+
+let divBottom = document.createElement("div")
+root.appendChild(divBottom)
+divBottom.innerHTML = iconCard
+
