080b62e1487646442856af6a92927bf47ec3512d
Verónica Rodríguez <veronikbw@gmail.com>
2025-08-29 19:21:08 +0000

    Proyecto terminado

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const inconsCard =  ["♦", "♥", "♠", "♣"]
 const numbersCard = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
 
+
 let body = document.body
 body.style.background = "green"
 body.style.height = "100vh"
@@ -11,22 +12,36 @@ body.style.display = "flex"
 body.style.alignItems = "center"
 body.style.justifyContent = "center"
 
+
 let root = document.getElementById("root");
 root.classList.add("my-card")
+
 
 let divTop = document.createElement("div")
 let iconCard = inconsCard[Math.floor(Math.random()*inconsCard.length)]
 root.appendChild(divTop)
 divTop.innerHTML = iconCard
-divTop.style.fontSize = "50px"
+divTop.style.fontSize = "140px"
+divTop.style.paddingLeft = "20px"
 
 
 let divMiddle = document.createElement("div")
 root.appendChild(divMiddle)
 divMiddle.innerHTML =  numbersCard[Math.floor(Math.random()*numbersCard.length)]
+divMiddle.style.fontSize = "200px"
+divMiddle.style.textAlign = "center"
 
 
 let divBottom = document.createElement("div")
 root.appendChild(divBottom)
 divBottom.innerHTML = iconCard
+divBottom.style.fontSize = "140px"
+divBottom.style.textAlign = "start"
+divBottom.style.paddingLeft = "20px"
+divBottom.style.transform = "rotate(180deg)"
 
+
+if (iconCard == "♦" || iconCard == "♥") {
+    divTop.style.color = "red"
+    divBottom.style.color = "red"
+}
