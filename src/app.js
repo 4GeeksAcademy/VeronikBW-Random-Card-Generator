881941e6c80d854f2c30767d251b191a6d4b92eb
Verónica Rodríguez <veronikbw@gmail.com>
2025-08-29 22:35:10 +0000

    agregar boton

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import "bootstrap";
 import "./style.css";
-const inconsCard =  ["♦", "♥", "♠", "♣"]
+const inconsCard = ["♦", "♥", "♠", "♣"]
 const numbersCard = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
 
@@ -11,6 +11,8 @@ body.style.height = "100vh"
 body.style.display = "flex"
 body.style.alignItems = "center"
 body.style.justifyContent = "center"
+body.style.display = "flex"
+body.style.flexDirection = "column"
 
 
 let root = document.getElementById("root");
@@ -18,7 +20,7 @@ root.classList.add("my-card")
 
 
 let divTop = document.createElement("div")
-let iconCard = inconsCard[Math.floor(Math.random()*inconsCard.length)]
+let iconCard = inconsCard[Math.floor(Math.random() * inconsCard.length)]
 root.appendChild(divTop)
 divTop.innerHTML = iconCard
 divTop.style.fontSize = "140px"
@@ -27,7 +29,7 @@ divTop.style.paddingLeft = "20px"
 
 let divMiddle = document.createElement("div")
 root.appendChild(divMiddle)
-divMiddle.innerHTML =  numbersCard[Math.floor(Math.random()*numbersCard.length)]
+divMiddle.innerHTML = numbersCard[Math.floor(Math.random() * numbersCard.length)]
 divMiddle.style.fontSize = "200px"
 divMiddle.style.textAlign = "center"
 
@@ -45,3 +47,16 @@ if (iconCard == "♦" || iconCard == "♥") {
     divTop.style.color = "red"
     divBottom.style.color = "red"
 }
+
+// let button = document.getElementById("button")
+// button.style.marginBottom = "50px"
+// button.addEventListener("click", () => {
+//     let iconCard = inconsCard[Math.floor(Math.random() * inconsCard.length)]
+//     divTop.innerHTML = iconCard
+//     divMiddle.innerHTML = numbersCard[Math.floor(Math.random() * numbersCard.length)];
+//     divBottom.innerHTML = iconCard;
+//     if (iconCard == "♦" || iconCard == "♥") {
+//         divTop.style.color = "red"
+//         divBottom.style.color = "red"
+//     }
+// }
